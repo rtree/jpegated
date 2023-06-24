ffbb60f2ef315d3ea59478784ac5c84a583ac503
rtree <arakitomohiko@gmail.com>
2023-06-24 01:35:59 -0400

    wondering on nextjs

---
+++ uinext/src/app/client.js
@@ -0,0 +1,32 @@
+// import useState from react
+import { useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+// import lit js sdk
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
+
+export default function Home() {
+  const [connected, setConnected] = useState()
+
+  async function connect() {
+
+    const client = new LitJsSdk.LitNodeClient({ alertWhenUnauthorized: false})
+
+    await client.connect();
+
+    if(client) {
+      console.log("Am Connected");
+    }
+
+    setConnected(true)
+  }
+  return (
+    <div className={styles.container}>
+      <h1>Hello</h1>
+      {
+        !connected && <button onClick={connect}>Connect</button>
+      }
+    </div>
+  )
+}
